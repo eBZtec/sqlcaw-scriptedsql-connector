9804ad2cff5056810129a7d043b33a86134e39fc
Jean M Santos <jean.michel@ebz.com.br>
2021-12-29 10:25:53 -0300

    configured schema for sqlcaw users table

--- SchemaScript.groovy
+++ SchemaScript.groovy
@@ -7,6 +7,10 @@ import org.identityconnectors.framework.common.objects.OperationalAttributes
 import org.identityconnectors.framework.spi.operations.SearchOp
 import org.identityconnectors.framework.common.objects.OperationOptionInfoBuilder
 
+import java.time.ZonedDateTime
+
+import static org.identityconnectors.framework.common.objects.AttributeInfo.Flags.REQUIRED
+
 def log = log as Log
 def operation = operation as OperationType
 def builder = builder as ICFObjectBuilder
@@ -17,25 +21,29 @@ log.info("Entering " + operation + " Script")
 builder.schema {
     objectClass {
         type ObjectClass.ACCOUNT_NAME
+        attribute OperationalAttributes.PASSWORD_NAME
+        attribute OperationalAttributes.ENABLE_NAME
         attributes {
-            cod_usu()
-            cod_grupo()
-            cod_parceiro()
-            cod_tipo_usu()
-            dat_alteraca_senha()
-            dat_ultimo_acesso()
-            dsc_email()
-            dsc_senha()
-            flg_ativo()
-            flg_usu_bloqueado()
-            id_usu()
-            idc_ctl_sen_exn()
-            nom_usu()
-            qtd_senha_invalida()
-            OperationalAttributes.PASSWORD_NAME
+            cod_grupo String.class
+            cod_parceiro String.class
+            cod_tipo_usu String.class
+            cod_usu String.class, REQUIRED
+            dat_alteracao_senha ZonedDateTime.class
+            dat_ultimo_acesso ZonedDateTime.class
+            dsc_email String.class
+            flg_ativo String.class
+            flg_usu_bloqueado String.class
+            id_usu String.class
+            idc_ctl_sen_exn String.class
+            nom_usu String.class
+            qtd_senha_invalida String.class
         }
     }
 
+    defineOperationOption OperationOptionInfoBuilder.buildPagedResultsCookie(), SearchOp
     defineOperationOption OperationOptionInfoBuilder.buildPagedResultsOffset(), SearchOp
     defineOperationOption OperationOptionInfoBuilder.buildPageSize(), SearchOp
+    defineOperationOption OperationOptionInfoBuilder.buildSortKeys(), SearchOp
+    defineOperationOption OperationOptionInfoBuilder.buildRunWithUser()
+    defineOperationOption OperationOptionInfoBuilder.buildRunWithPassword()
 }

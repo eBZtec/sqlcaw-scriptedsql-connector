b46c26fa6cf341904ff3f1368a4e160b2b2a99cb
Jean M Santos <jean.michel@ebz.com.br>
2021-12-29 09:47:38 -0300

    added lib imports for create scripts

--- CreateScript.groovy
+++ CreateScript.groovy
@@ -1,5 +1,21 @@
 import groovy.sql.Sql;
-import groovy.sql.DataSet
+import org.forgerock.openicf.connectors.scriptedsql.ScriptedSQLConfiguration
+import org.forgerock.openicf.misc.scriptedcommon.OperationType
+import org.identityconnectors.common.logging.Log
+import org.identityconnectors.framework.common.objects.Attribute
+import org.identityconnectors.framework.common.objects.ObjectClass
+import org.identityconnectors.framework.common.objects.OperationOptions
+
+import java.sql.Connection
+
+def log = log as Log
+def operation = operation as OperationType
+def options = options as OperationOptions
+def objectClass = objectClass as ObjectClass
+def attributes = attributes as Set<Attribute>
+def connection = connection as Connection
+def id = id as String
+def configuration = configuration as ScriptedSQLConfiguration
 
 log.info("Entering " + operation + " Script");
 
